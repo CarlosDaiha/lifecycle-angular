e95ae27ff229fb90fb7e90bdcd9815a143759830
Carlos Daiha Basna <carlos.daiha-basna@capgemini.com>
2021-12-13 21:03:44 +0100

    first commit

---
+++ src/app/pages/pagina1/pagina1.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina1',
+  templateUrl: './pagina1.component.html',
+  styles: [
+  ]
+})
+export class Pagina1Component implements OnInit, OnChanges, OnDestroy {
+  
+  guardar() {
+
+  }
+
+  nombre:string = "Carlos"; 
+
+  constructor() { }
+  
+  ngOnInit(): void {
+    console.log("ngOnInit")
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("ngOnChanges")
+  }
+  // ngDoCheck(): void {
+  //   console.log("ngDoCheck")
+  // }
+  // ngAfterContentInit(): void {
+  //   console.log("ngAfterContentInit")
+  // }
+  // ngAfterContentChecked(): void {
+  //   console.log("ngAfterContentChecked")
+  // }
+  // ngAfterViewInit(): void {
+  //   console.log("ngAfterViewInit")
+  // }
+  // ngAfterViewChecked(): void {
+  //   console.log("ngAfterViewChecked")
+  // }
+  ngOnDestroy(): void {
+    console.log("ngOnDestroy")
+  }
+
+}
